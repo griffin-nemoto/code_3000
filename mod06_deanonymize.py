3d112af43290c35aea18d69a8fcd9f896446c6b4
Griffin Nemoto <gtn23001@uconn.edu>
2026-03-11 17:00:13 -0400

    mod6 done

--- mod06_deanonymize.py
+++ mod06_deanonymize.py
@@ -18,7 +18,14 @@ def link_records(anon_df, aux_df):
       anon_id, matched_name
     containing ONLY uniquely matched records.
     """
-    raise NotImplementedError
+    identifiers = ['zip3','age','gender']
+    merge = pd.merge(anon_df,aux_df,on=identifiers,how='inner')
+    
+    match_anon = merge.groupby('anon_id')['anon_id'].transform('count')
+    match_aux = merge.groupby('name')['name'].transform('count')
+    
+    unique = merge[(match_anon ==1) and (match_aux ==1)].copy()
+    return unique[['anon_id','name']].rename(columns={'name':'match'})
 
 
 def deanonymization_rate(matches_df, anon_df):
@@ -26,4 +33,6 @@ def deanonymization_rate(matches_df, anon_df):
     Compute the fraction of anonymized records
     that were uniquely re-identified.
     """
-    raise NotImplementedError
+    if len(anon_df) == 0:
+        return 0
+    return len(matches_df) / len(matches_df)
